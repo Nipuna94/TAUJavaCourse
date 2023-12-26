734ea18bf3c632a5a6a20a8ec7677f95a22cde41
Nipuna94 <ninurocket@gmail.com>
2023-12-26 22:38:06 +0530

    Methods

--- src/chapter5/PhoneBillCalculator.java
+++ src/chapter5/PhoneBillCalculator.java
@@ -16,13 +16,13 @@ public class PhoneBillCalculator {
 
         double overageTimeFee = calculateOverageTimeFee(overageTime);
 
-        double totalBeforeTax = calculateTotalBeforeTax(overageTimeFee,planFee);
+        double totalBeforeTax = calculateTotalBeforeTax(overageTimeFee, planFee);
 
         double taxedAmount = taxAmount(totalBeforeTax);
 
-        double totalBill = totalAfterTax(totalBeforeTax,taxedAmount);
+        double totalBill = totalAfterTax(totalBeforeTax, taxedAmount);
 
-        printBill(planFee,overageTimeFee,taxedAmount,totalBill);
+        printBill(planFee, overageTimeFee, taxedAmount, totalBill);
 
     }
 
@@ -33,40 +33,45 @@ public class PhoneBillCalculator {
         double planFee = scanner.nextDouble();
         return planFee;
     }
+
     //get the overage time from user
     public static double getOverageTime() {
         System.out.println(" Enter overage minutes: ");
         double overageTime = scanner.nextDouble();
-        return  overageTime;
+        return overageTime;
     }
+
     //calculate overage fee
     public static double calculateOverageTimeFee(double overageTime) {
-        double overageTimeFee = overageTime*chargePerMinute;
+        double overageTimeFee = overageTime * chargePerMinute;
         return overageTimeFee;
     }
+
     //calculate total before the tax
-    public static double calculateTotalBeforeTax(double overageTimeFee,double planFee) {
-        double totalBeforeTax = overageTimeFee+planFee;
+    public static double calculateTotalBeforeTax(double overageTimeFee, double planFee) {
+        double totalBeforeTax = overageTimeFee + planFee;
         return totalBeforeTax;
     }
+
     //calculate tax amount
     public static double taxAmount(double totalBeforeTax) {
-        double taxAmount = totalBeforeTax*tax;
+        double taxAmount = totalBeforeTax * tax;
         return taxAmount;
     }
+
     //calculate total
     public static double totalAfterTax(double totalBeforeTax, double taxedAmount) {
-        double totalAfterTax = totalBeforeTax+taxedAmount;
+        double totalAfterTax = totalBeforeTax + taxedAmount;
         return totalAfterTax;
     }
 
     //print the invoice
-    public static void printBill(double planFee, double overageTimeFee,double taxedAmount,double totalBill){
+    public static void printBill(double planFee, double overageTimeFee, double taxedAmount, double totalBill) {
         System.out.println("Phone Bill Statement");
-        System.out.println("Plan: $" + String.format("%.2f",planFee));
-        System.out.println("Overage: $" + String.format("%.2f",overageTimeFee));
-        System.out.println("Tax: $" +  String.format("%.2f",taxedAmount));
-        System.out.println("Total: $" + String.format("%.2f",totalBill));
+        System.out.println("Plan: $" + String.format("%.2f", planFee));
+        System.out.println("Overage: $" + String.format("%.2f", overageTimeFee));
+        System.out.println("Tax: $" + String.format("%.2f", taxedAmount));
+        System.out.println("Total: $" + String.format("%.2f", totalBill));
     }
 
 }
